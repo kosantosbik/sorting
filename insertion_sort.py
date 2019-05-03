dee403fbd15d4dd592aabb65c6a04396ddff306f
kosantosbik <bastugn@itu.edu.tr>
2019-05-03 21:49:32 +0300

    Implemented insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,9 @@
+def insertion_sort(arr):
+    "Sorts the given array using insertion sort algorithm"
+
+    for i in range(1, len(arr)):
+        for j in range(i):
+            if arr[i] < arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+
+    return arr
